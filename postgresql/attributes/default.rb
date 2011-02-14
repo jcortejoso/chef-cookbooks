9da3aeb4b927f058bd5eaad224b0fed39f403e9b
Robert R Evans <robert@codewranglers.org>
2011-02-14 12:22:35 -0800

    Updated postgres

--- postgresql/attributes/default.rb
+++ postgresql/attributes/default.rb
@@ -28,12 +28,12 @@ when "debian"
   set[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
 
 when "ubuntu"
-
-  if platform_version.to_f <= 9.04
-    default[:postgresql][:version] = "8.3"
-  else
-    default[:postgresql][:version] = "8.4"
-  end
+  default[:postgresql][:version] = "8.3"
+  # if platform_version.to_f <= 9.04
+  #   default[:postgresql][:version] = "8.3"
+  # else
+  #   default[:postgresql][:version] = "8.4"
+  # end
 
   set[:postgresql][:dir] = "/etc/postgresql/#{node[:postgresql][:version]}/main"
 
